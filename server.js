cf8b295651063ee749c51da79a5fc261c35b911a
hao nguyen <huuhao1999@gmail.com>
2019-12-21 16:42:18 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express'),
     app = express(),
     exphbs = require('express-handlebars');
 const dotEnv = require('dotenv');
-
-
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var config            =     require('./config/config')
+const FacebookStrategy  =     require('passport-facebook').Strategy
+require('./config/passport')(passport);
 //config cho express handlebars
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
 
 dotEnv.config();
 const PORT = process.env.PORT || 3000
@@ -21,9 +34,17 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 
+app.use(session({
+	secret: 'vidyapathaisalwaysrunning',
+	resave: true,
+	saveUninitialized: true
+ } ));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 //điều hướng về controller
 app.use('/', require('./routers'));
-
+app.use(express.static(__dirname + '/public'));
 app.listen(PORT, () => {
     console.log(`Server listening at PORT: ${PORT}`)
 });
