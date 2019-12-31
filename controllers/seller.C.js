94bba9bcb77977d0f3539bd83d7358cd16972203
MVT <mangvientrieu@gmail.com>
2019-12-31 11:28:36 +0700

    Edit Description

--- controllers/seller.C.js
+++ controllers/seller.C.js
@@ -4,13 +4,13 @@ module.exports = {
     {
         res.render('./vwSeller/upload_file.hbs');
         var pro = req.body;
-        Seller.add(1, pro.product_name, pro.product_name, pro.product_description, pro.starting_price, pro.step_price, pro.price_tobuynow, 1, pro.extension);
+        var date = new Date().toString();
+        Seller.add(1, pro.product_name, pro.product_name, '<b>'+date+'</b>' + pro.product_description, pro.starting_price, pro.step_price, pro.price_tobuynow, 1, pro.extension);
     },
     LoadPro: async function(req, res) 
     {
         try {
             const Table = await Seller.load(1);
-            console.log(Table);
             res.render("./vwSeller/myProducts", {
                 title: "My Product",
                 Table: Table
