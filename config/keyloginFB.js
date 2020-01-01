73e816848635cfba3c6a7100108b130a1d397250
Hảo <huuhao1999@gmail.com>
2020-01-01 11:56:57 +0700

    key fb

---
+++ config/keyloginFB.js
@@ -0,0 +1,5 @@
+module.exports={
+  "facebook_api_key"      :     "560997384450038",
+  "facebook_api_secret"   :     "f5639a692a0ec017df5fbad497eba965",
+  "callback_url"          :     "http://localhost:3000/auth/facebook/callback"
+}
